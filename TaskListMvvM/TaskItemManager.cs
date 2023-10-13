ea3250b31755ba159f0bfe3a222f30d2286c3177
stb.infoserv <stb.infoserv@gmail.com>
2023-10-13 12:57:14 +0300

    set TaskItemManager as a static class

--- TaskListMvvM/TaskItemManager.cs
+++ TaskListMvvM/TaskItemManager.cs
@@ -1,88 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+
 
 namespace TaskListMvvM
 {
-    public class TaskItemManager
+    public static class TaskItemManager
     {
-        public List<TaskItem> GetTasks()
+        public static List<TaskItem> GetTasks()
         {
-            using (var context = new TaskListContext())
-            {
-                return context.Tasks.ToList();
-            }
+            using var context = new TaskListContext();
+            return context.Tasks.ToList();
         }
 
-        public void AddTask(TaskItem task)
+        public static void AddTask(TaskItem task)
         {
-            using (var context = new TaskListContext())
-            {
-                context.Tasks.Add(task);
-                context.SaveChanges();
-            }
+            using var context = new TaskListContext();
+            context.Tasks.Add(task);
+            context.SaveChanges();
         }
 
-        public void UpdateTask(TaskItem taskItem)
+        public static void UpdateTask(TaskItem taskItem)
         {
-            using (var context = new TaskListContext())
-            {
-                if (taskItem != null)
-                {
-                    var task = context.Tasks.Find(taskItem.Id);
-                    if (task != null)
-                    {
-                        if (task.IsCompleted)
-                            task.IsCompleted = false;
-                        else
-                            task.IsCompleted = true;
-                        context.SaveChanges();
-                    }
-                }
-            }
+            using var context = new TaskListContext();
+            var task = context.Tasks.Find(taskItem?.Id);
+            if (task == null) return;
+            task.IsCompleted = !task.IsCompleted;
+            context.SaveChanges();
         }
 
-        public bool DeleteTask(TaskItem task)
+        public static bool DeleteTask(TaskItem task)
         {
-            using (var context = new TaskListContext())
-            {
-                if (task is not null)
-                {
-                    context.Tasks.Remove(task);
-                    context.SaveChanges();
-                    return true;
-                }
-                return false;
-            }
+            using var context = new TaskListContext();
+            if (task is null) return false;
+            context.Tasks.Remove(task);
+            context.SaveChanges();
+            return true;
         }
 
-        public void Clear()
+        public static void Clear()
         {
-            using (var context = new TaskListContext())
-            {
-                context.Database.EnsureDeleted();
-            }
+            using var context = new TaskListContext();
+            context.Database.EnsureDeleted();
         }
 
-        public void ChangeTaskDescription(TaskItem taskItem, string title)
+        public static void ChangeTaskDescription(TaskItem taskItem, string title)
         {
-            using (var context = new TaskListContext())
-            {
-                if (taskItem is not null)
-                {
-                    var task = context.Tasks.Find(taskItem.Id);
-                    if (task is not null)
-                    {
-                        task.Title = title;
-                        context.SaveChanges();
-                    }
-
-                }
-            }
+            using var context = new TaskListContext();
+            if (taskItem is null) return;
+            var task = context.Tasks.Find(taskItem.Id);
+            if (task is null) return;
+            task.Title = title;
+            context.SaveChanges();
         }
     }
 }
