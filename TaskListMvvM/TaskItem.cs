aa7005b9e138bb1e095c0d3e3d0579d60fe18627
Nicklackis <96375900+nicklakiss@users.noreply.github.com>
2023-11-04 19:27:47 +0200

    Added date

--- TaskListMvvM/TaskItem.cs
+++ TaskListMvvM/TaskItem.cs
@@ -11,5 +11,6 @@ namespace TaskListMvvM
         public int Id { get; set; }
         public string? Title { get; set; }
         public bool IsCompleted { get; set; }
+        public DateTime Date { get; set; }
     }
 }
